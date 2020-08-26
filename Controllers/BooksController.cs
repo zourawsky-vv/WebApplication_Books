6c4c6830bf906ffda9e6f5ddf7e20d59da3a334c
Vladimir <zourawsky@yandex.ru>
2020-08-26 18:41:19 +0300

    Добавлена страница Books и информация на ней. Создана Модель - класс С#, Контроллер - объект коллекции на основе класса, Вид - визуализация объекта.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication_Books.Models;
+
+namespace WebApplication_Books.Controllers
+{
+    public class BooksController : Controller
+    {
+        private List<Book> books;
+
+        public BooksController() => books = new List<Book>
+            {
+                new Book {Title = "Book 1", Price = 100, Count = 09},
+                new Book {Title = "Book 2", Price = 200, Count = 11}
+            };
+        public IActionResult Books()
+        {
+            return View(books);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Books.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Decimal Price { get; set; }
+        public int Count { get; set; }
+    }
+}
