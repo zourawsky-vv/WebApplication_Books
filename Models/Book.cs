8531a6fbecd0d8956c261be7d39b3ab0ea80e11a
Vladimir <zourawsky@yandex.ru>
2020-09-06 19:58:48 +0300

    Обновление модели.

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication_Books.Models
 {
-    public class BookCount
-    {
-        public int ID { get; set; }
-        public int Count { get; set; }
-    }
+    //public class BookCount
+    //{
+    //    public int ID { get; set; }
+    //    public int Count { get; set; }
+    //}
     public class Book
     {
         public int ID { get; set; }
+
+        [Required, Display(Name = "Название"), StringLength(100, MinimumLength = 3, ErrorMessage = "Длина строки должна быть 3-100.")]
         public string Title { get; set; }
+
+        [Display(Name = "Стоимость"), Column(TypeName = "decimal(18, 0)"), DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        
         public int Count { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace WebApplication_Books.Models
 {
     public class User : IdentityUser
     {
+        [Required, Display(Name = "Пользователь"), StringLength(256, MinimumLength = 3)]
         public string FirstName { get; set; }
     }
 }
